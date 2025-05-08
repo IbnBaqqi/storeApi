a636694440bc8491fc7e9bf74aa293ecbdcfef95
Abdulbaki <Salausmart132@gmail.com>
2025-05-08 21:44:35 +0300

    built Product endpoints

---
+++ src/main/java/com/salausmart/store/mappers/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.salausmart.store.mappers;
+
+import com.salausmart.store.dtos.ProductDto;
+import com.salausmart.store.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(source = "category.id", target = "categoryId")
+    ProductDto productToProductDto(Product product);
+}
