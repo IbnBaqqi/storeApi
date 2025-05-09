83dc58df462999ab77a488f8b2ed83ad6862332b
Abdulbaki <Salausmart132@gmail.com>
2025-05-09 08:34:38 +0300

    added create user mapper (toEntity)

--- src/main/java/com/salausmart/store/mappers/UserMapper.java
+++ src/main/java/com/salausmart/store/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.salausmart.store.mappers;
 
+import com.salausmart.store.dtos.RegisterUserRequest;
 import com.salausmart.store.dtos.UserDto;
 import com.salausmart.store.entities.User;
 import org.mapstruct.Mapper;
@@ -7,4 +8,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+    User toEntity(RegisterUserRequest request);
 }
