04cda8eb1fb7d96cc2310113ec58e07c2eb2a03e
Abdulbaki <Salausmart132@gmail.com>
2025-05-12 10:40:20 +0300

    added carts entities and relationship

---
+++ src/main/java/com/salausmart/store/entities/Cart.java
@@ -0,0 +1,30 @@
+package com.salausmart.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "date_created", insertable = false, updatable = false)
+    private LocalDate dateCreated;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartItem> cartItem = new HashSet<>();
+}
---
+++ src/main/java/com/salausmart/store/entities/CartItem.java
@@ -0,0 +1,32 @@
+package com.salausmart.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
