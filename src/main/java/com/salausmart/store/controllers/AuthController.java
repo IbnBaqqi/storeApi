aca9f61eb666b0f98126f9492f8b0cf36d1ef76d
Abdulbaki <Salausmart132@gmail.com>
2025-05-16 23:58:04 +0300

    issuing Refresh Tokens

--- src/main/java/com/salausmart/store/controllers/AuthController.java
+++ src/main/java/com/salausmart/store/controllers/AuthController.java
@@ -6,6 +6,8 @@ import com.salausmart.store.dtos.UserDto;
 import com.salausmart.store.mappers.UserMapper;
 import com.salausmart.store.repositories.UserRepository;
 import com.salausmart.store.services.JwtService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,16 +29,24 @@ public class AuthController {
     private final UserMapper userMapper;
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
         );
 
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
 
-        var token = jwtService.generateToken(user);
+        var accessToken = jwtService.generateAccessToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
 
-        return ResponseEntity.ok(new JwtResponse(token));
+        var cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true); // cannot be accessed by javascript
+        cookie.setPath("/auth/refresh");
+        cookie.setMaxAge(604800); // 7 days
+        cookie.setSecure(true);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok(new JwtResponse(accessToken));
     }
 
     @GetMapping("/me")
