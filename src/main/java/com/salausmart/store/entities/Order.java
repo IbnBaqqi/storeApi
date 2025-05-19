7e9935a3731521790415cc8708c8e465d0b42439
Abdulbaki <Salausmart132@gmail.com>
2025-05-19 19:36:54 +0300

    create entities for orders & order_items table

---
+++ src/main/java/com/salausmart/store/entities/Order.java
@@ -0,0 +1,43 @@
+package com.salausmart.store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+
+    @OneToMany(mappedBy = "orderId")
+    private Set<OrderItem> items = new LinkedHashSet<>();
+}
---
+++ src/main/java/com/salausmart/store/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.salausmart.store.entities;
+
+public enum OrderStatus {
+    PENDING, PAID, FAILED, CANCELLED
+}
