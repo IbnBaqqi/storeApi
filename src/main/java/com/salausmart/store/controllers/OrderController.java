ffbaa50b83d101f4ffaee0432c10ef738a99c30a
Abdulbaki <Salausmart132@gmail.com>
2025-05-21 22:45:20 +0300

    getOrder endpoint refactor

--- src/main/java/com/salausmart/store/controllers/OrderController.java
+++ src/main/java/com/salausmart/store/controllers/OrderController.java
@@ -1,16 +1,12 @@
 package com.salausmart.store.controllers;
 
 import com.salausmart.store.dtos.OrderDto;
-import com.salausmart.store.mappers.OrderMapper;
-import com.salausmart.store.repositories.OrderRepository;
-import com.salausmart.store.services.AuthService;
+import com.salausmart.store.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
@@ -18,19 +14,10 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
 
-    private final AuthService authService;
-    private final OrderRepository orderRepository;
-    private final OrderMapper orderMapper;
+    private final OrderService orderService;
 
     @GetMapping
-    public ResponseEntity<?> getOrder() {
-        var user = authService.getCurrentUser();
-        var order = orderRepository.getOrdersWithItemsByCustomerId(user.getId());
-        List<OrderDto> orderDtos = new ArrayList<>();
-
-        order.forEach(order1 -> {
-            orderDtos.add(orderMapper.toDto(order1));
-        });
-        return ResponseEntity.ok(orderDtos);
+    public List<OrderDto> getAllOrder() {
+        return orderService.getAllOrder();
     }
 }
--- src/main/java/com/salausmart/store/repositories/OrderRepository.java
+++ src/main/java/com/salausmart/store/repositories/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.salausmart.store.repositories;
 
 import com.salausmart.store.entities.Order;
+import com.salausmart.store.entities.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @EntityGraph(attributePaths = "items.product")
     @Query("select o from Order o where o.customer.id = ?1")
     List<Order> getOrdersWithItemsByCustomerId(Long customerId);
+
+    @EntityGraph(attributePaths = "items.product")
+    @Query("select o from Order o where o.customer = ?1")
+    List<Order> getAllByCustomer(User customer);
 }
