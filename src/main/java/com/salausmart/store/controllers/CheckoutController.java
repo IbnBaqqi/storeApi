dc5f4b9c0cf286f2652003178e29761c555cb625
Abdulbaki <Salausmart132@gmail.com>
2025-05-22 16:32:18 +0300

    added webhook endpoint

--- src/main/java/com/salausmart/store/controllers/CheckoutController.java
+++ src/main/java/com/salausmart/store/controllers/CheckoutController.java
@@ -7,22 +7,56 @@ import com.salausmart.store.exceptions.CartEmptyException;
 import com.salausmart.store.exceptions.CartNotFoundException;
 import com.salausmart.store.exceptions.PaymentException;
 import com.salausmart.store.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/checkout")
 public class CheckoutController {
 
     private final CheckoutService checkoutService;
 
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
+
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
             return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(
+            @RequestHeader("Stripe-Signature") String signature,
+            @RequestBody String payload) {
+        try {
+            var event = Webhook.constructEvent(payload, signature, webhookSecretKey);
+            System.out.println(event.getType());
+
+            var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+            // charge -> (Charge) stripeObject
+            // payment_intent.succeeded -> (PaymentIntent) stripeObject
+
+            switch (event.getType()) {
+                case "payment_intent.succeeded" -> {
+                    // update order status (PAID)
+                }
+                case "payment_intent.failed" -> {
+                    // update order status (FAILED)
+                }
+            }
+
+            return ResponseEntity.ok().build();
+
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
